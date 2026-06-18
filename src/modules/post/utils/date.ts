63c363b75e40bad4b8282e0c7cdcfab987474afa
kangcommit <ahmad.codespace@gmail.com>
2026-06-18 21:53:33 +0700

    feat: add date utilities

---
+++ src/modules/post/utils/date.ts
@@ -0,0 +1,9 @@
+export function formatPostDate(date: string | Date) {
+	return new Date(date)
+		.toLocaleDateString("en-US", {
+			month: "short",
+			day: "numeric",
+			year: "numeric",
+		})
+		.toUpperCase();
+}
