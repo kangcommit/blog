d8a97d724e63aa7ceaaf1e2a6968a38f89e7ef63
kangcommit <ahmad.codespace@gmail.com>
2026-06-18 17:50:12 +0700

    refactor: rename types.tsx to types.ts and update Post interface

---
+++ src/modules/post/types.ts
@@ -0,0 +1,34 @@
+export interface PostResponse {
+	message: string;
+	data: Post;
+}
+
+export interface PostsResponse {
+	message: string;
+	data: Post[];
+}
+
+export interface Post {
+	id: number;
+	title: string;
+	slug: string;
+	content: string;
+	excerpt: string;
+	status: string;
+	createdAt: string;
+}
+
+export interface CreatePostInput {
+	title: string;
+	slug: string;
+	content: string;
+	excerpt: string;
+}
+
+export type UpdatePostInput = Partial<CreatePostInput>;
+
+export type PostFormFields = {
+	title: string;
+	excerpt: string;
+	content: string;
+};
